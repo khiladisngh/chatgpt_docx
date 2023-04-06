ac412eaa268d72c8698dd81828c51378d36be0b5
Gishant Singh <khiladisngh@hotmail.com>
2023-04-06 19:39:10 +0530

    update: added changes

--- src/chatgpt_to_doc.py
+++ src/chatgpt_to_doc.py
@@ -1,8 +1,7 @@
 import os
 import openai
 import docx
-
-
+import time
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 from docx.shared import Pt
 from termcolor import colored
@@ -13,20 +12,30 @@ load_dotenv()
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
 
-def generate_text(prompt):
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=prompt,
-        max_tokens=200,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
+def generate_text(prompt, retries=3):
+    for i in range(retries):
+        try:
+            response = openai.Completion.create(
+                engine="text-davinci-002",
+                prompt=prompt,
+                max_tokens=200, # Try to keep this below 200
+                n=1,
+                stop=None,
+                temperature=0.5,
+            )
 
-    return response.choices[0].text.strip()
+            return response.choices[0].text.strip()
+
+        except openai.error.APIError as e:
+            print(f"OpenAI API error ({e.status}): {e.message}")
+            if i == retries - 1:
+                raise e
+            wait_time = 2 ** i
+            print(f"Retrying in {wait_time} seconds...")
+            time.sleep(wait_time)
 
 
-def save_to_doc_file(filename, title, content):
+def save_to_doc_file(filename, title, content, directory="output"):
     doc = docx.Document()
 
     # Add title
@@ -43,26 +52,46 @@ def save_to_doc_file(filename, title, content):
         bullet_run = bullet_paragraph.add_run(line)
         bullet_run.font.size = Pt(12)
 
-    doc.save(filename)
+    # Directory to save the file to
+    filename = os.path.join(directory, filename)
+
+    # Check if the directory exists, create it if it doesn't
+    if not os.path.exists(directory):
+        os.mkdir(directory)
+
+    # Save to docx file. Create numbered file if file already exists
+    if os.path.exists(filename):
+        i = 1
+        while os.path.exists(f"{filename[:-5]}_{i}.docx"):
+            i += 1
+        doc.save(f"{filename[:-5]}_{i}.docx")
+    else:
+        doc.save(filename)
 
 
 if __name__ == "__main__":
     # Get user input
-    topic = input("Enter a topic: ")
-    adjective = input("Enter an adjective (e.g., intriguing, funny, sad): ")
-    num_facts = input("Enter the number of facts (e.g., 5, 10, 20): ")
+    topic = input("Enter a topic (default: 'Cars'): ") or 'Cars'
+    adjective = input(
+        "Enter an adjective (default: 'interesting'): ") or 'interesting'
+    num_facts = input("Enter the number of facts (default: 10): ") or '10'
     format_ = input(
-        "Enter the format (e.g., list, paragraph, bullet points): ")
+        "Enter the format (default: 'list'): ") or 'list'
     importance = input(
-        "Enter the importance (e.g., most important, least important, random): ")
-    category = input(
-        "Enter the category (e.g., history, nutrition, sports, uses, etc.): ")
-    simplify = input("Simplify the text? (y/n): ")
+        "Enter the importance (default: 'random'): ") or 'random'
+    category = input("Enter the category (default: 'None'): ")
+    simplify = input("Simplify the text? (y/n) (default: n): ") or 'n'
 
     # Generate text
-    prompt = f"Write {num_facts} {adjective} {importance} facts about {topic} in the {category} category in {format_} format:\n\n"
+    prompt = f"Write {num_facts} {adjective} {importance} facts about {topic} "
+    if category:
+        prompt += f"in the category: {category} "
+    if format_:
+        prompt += f"in the format: {format_} "
     if simplify == "y":
         prompt += "\nSimplify the text."
+    else:
+        prompt += "\nDon't simplify the text."
 
     generated_text = generate_text(prompt)
 
@@ -72,11 +101,17 @@ if __name__ == "__main__":
     result = "\n".join(lines)
 
     # Save to doc file
-    output_filename = "chatgpt_output.docx"
+    # Create a separate docx file for the topic
+    output_filename = f"{topic.replace(' ', '_').lower()}_facts.docx"
+
+    # Save generated text to docx file
     save_to_doc_file(
         output_filename, f"{num_facts} {adjective.title()} {importance.title()} Facts About {topic.title()} ({category.title()})", result)
 
     # Print results
-    print(colored(f"Generated text saved to {output_filename}\n", "green"))
+    print(colored(f"\nGenerated text saved to {output_filename}", "green"))
+    print("Prompt:\n")
+    print(colored(prompt, "yellow"))
+    print("\n")
     print("Generated content:\n")
     print(colored(generated_text, "cyan"))
